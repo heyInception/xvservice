97dd65f238a1b2bcad53ed5ff9e7a6f1567b5252
Anton Schastlivtsev <antonschastlivc@gmail.com>
2024-07-11 15:14:27 +0400

    fixed

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,15 +1,8 @@
  // tabbed content
  // http://www.entheosweb.com/tutorials/css/tabs.asp
- $(".header__wrap").hide();
- $(".header__wrap:first").show();
- $(".fixed__menu").hide();
- $(".fixed__menu:first").show();
+
  /* if in tab mode */
  $(".header__tabs li").click(function() {
-     let wrap = $(".header__wrap")
-     let wrapMobile = $(".fixed__menu")
-     wrap.hide();
-     wrapMobile.hide();
      var activeTab = $(this).attr("rel");
      $("#" + activeTab).fadeIn();
      console.log(activeTab);
